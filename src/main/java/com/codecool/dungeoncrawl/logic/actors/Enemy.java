87944f99ffaace04c96b6d946ad1a6f55ca6408d
Csilla Lukacs <erzsebet.lk@gmail.com>
2021-09-14 10:18:23 +0200

    create Enemy abstract class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Enemy.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Enemy.java
@@ -2,15 +2,15 @@ package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 
-public class Skeleton extends Enemy {
-    public Skeleton(Cell cell) {
+public abstract class Enemy extends Actor{
+
+    public Enemy(Cell cell) {
         super(cell);
     }
 
     @Override
     public String getTileName() {
-        return "skeleton";
+        return "enemy";
     }
-
 
 }
