8affdc38f4d5e481f121b347f3297d4f7ed2ef24
Csilla Lukacs <erzsebet.lk@gmail.com>
2021-09-14 13:12:38 +0200

    add movement to wizard

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Wizard.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Wizard.java
@@ -1,8 +1,11 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 
 public class Wizard extends Enemy{
+
+    private char movementDirection = 'h';
 
     public Wizard(Cell cell) {
         super(cell);
@@ -13,6 +16,29 @@ public class Wizard extends Enemy{
         return "wizard";
     }
 
+    public int[] getNextMove(){
+
+        int d = (int) Math.signum(0.5  - Math.random());
+
+        if (movementDirection == 'h'){
+            return new int[]{d, 0};
+        } else {
+            return new int[]{0, d};
+        }
+    }
+
+    @Override
+    public void move(int dx, int dy){
+        Cell nextCell = this.cell.getNeighbor(dx, dy);
+        if (nextCell.getActor() == null
+                && nextCell.getType() != CellType.WALL
+                && nextCell.getItem() == null
+        ) {
+            super.move(dx, dy);
+        }
+    }
+
+   @Override
     public void act(){
         for (int dx = -2; dx < 3; dx++) {
             for (int dy = -2; dy < 3; dy++){
@@ -20,6 +46,9 @@ public class Wizard extends Enemy{
                     Actor actor = cell.getNeighbor(dx, dy).getActor();
                     if (actor instanceof Player){
                         attack(actor);
+                    } else {
+                        int[] move = getNextMove();
+                        move(move[0], move[1]);
                     }
                 }
             }
