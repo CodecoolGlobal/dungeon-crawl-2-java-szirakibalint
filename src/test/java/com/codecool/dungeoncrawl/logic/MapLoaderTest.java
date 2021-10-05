32a379aec1129af386e35fd206d32ac867eef459
Balint Kozari <mrkoba@gmail.com>
2021-10-05 16:25:20 +0200

    feat: create two maploader tests #24

--- src/test/java/com/codecool/dungeoncrawl/logic/MapLoaderTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/MapLoaderTest.java
@@ -1,2 +1,34 @@
-package com.codecool.dungeoncrawl.logic;public class MapLoaderTest {
+package com.codecool.dungeoncrawl.logic;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MapLoaderTest {
+
+    @Test
+    void loadMap_LoadDummyMap_NoExceptionsThrown() {
+        assertAll(() -> MapLoader.loadMap("/testmap.txt"));
+    }
+
+    @Test
+    void loadMap_LoadDummyMap_EveryCellHasProperTile() {
+        GameMap gameMap = MapLoader.loadMap("/testmap.txt");
+        Cell empty = gameMap.getCell(0, 0);
+        Cell floor = gameMap.getCell(3, 1);
+        Cell wall = gameMap.getCell(1, 1);
+        Cell closedDoor = gameMap.getCell(4, 1);
+        Cell redFlower = gameMap.getCell(4, 3);
+        Cell yellowFlower = gameMap.getCell(2, 4);
+        Cell heartOnFloor = gameMap.getCell(3, 4);
+        assertEquals(CellType.EMPTY, empty.getType());
+        assertEquals(CellType.FLOOR, floor.getType());
+        assertEquals(CellType.WALL, wall.getType());
+        assertEquals(CellType.CLOSEDDOOR, closedDoor.getType());
+        assertEquals(CellType.REDFLOWER, redFlower.getType());
+        assertEquals(CellType.YELLOWFLOWER, yellowFlower.getType());
+        assertEquals(CellType.HEARTONFLOOR, heartOnFloor.getType());
+    }
+
 }
