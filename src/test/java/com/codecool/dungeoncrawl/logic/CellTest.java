11614cf31b7e3813c827b48ef89d297870b98535
Balint Kozari <mrkoba@gmail.com>
2021-10-05 11:18:41 +0200

    feat: add cell tests #24

--- src/test/java/com/codecool/dungeoncrawl/logic/CellTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/CellTest.java
@@ -1,15 +1,24 @@
 package com.codecool.dungeoncrawl.logic;
 
+import com.codecool.dungeoncrawl.logic.items.Item;
+import com.codecool.dungeoncrawl.logic.items.Sword;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CellTest {
-    GameMap map = new GameMap(3, 3, CellType.FLOOR);
+    GameMap map;
+    Cell cell;
+
+    @BeforeEach
+    void setup() {
+        map = new GameMap(3, 3, CellType.FLOOR);
+        cell = map.getCell(1, 1);
+    }
 
     @Test
     void getNeighbor() {
-        Cell cell = map.getCell(1, 1);
         Cell neighbor = cell.getNeighbor(-1, 0);
         assertEquals(0, neighbor.getX());
         assertEquals(1, neighbor.getY());
@@ -22,5 +31,30 @@ class CellTest {
 
         cell = map.getCell(1, 2);
         assertEquals(null, cell.getNeighbor(0, 1));
+    }
+
+    @Test
+    void getItem_NoItemOnCell_ReturnNull() {
+        assertNull(cell.getItem());
+    }
+
+    @Test
+    void getItem_ItemSetOnCell_ReturnItem() {
+        Item item = new Sword(cell);
+        cell.setItem(item);
+        assertEquals(item, cell.getItem());
+    }
+
+    @Test
+    void getTileName_Floor_ReturnFloor() {
+        String expectedTileName = "floor";
+        assertEquals(expectedTileName, cell.getTileName());
+    }
+
+    @Test
+    void getTileName_Wall_ReturnWall() {
+        cell.setType(CellType.WALL);
+        String expectedTileName = "wall";
+        assertEquals(expectedTileName, cell.getTileName());
     }
 }
