dc7a9c38a0a58a4a626b4eddded71dd114ee5fea
Balint Kozari <mrkoba@gmail.com>
2021-10-05 19:28:54 +0200

    feat: add more maploader tests

--- src/test/java/com/codecool/dungeoncrawl/logic/MapLoaderTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/MapLoaderTest.java
@@ -1,9 +1,19 @@
 package com.codecool.dungeoncrawl.logic;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class MapLoaderTest {
+
+    GameMap gameMap;
+
+    @BeforeEach
+    void setupMap() {
+        gameMap = MapLoader.loadMap("/testmap.txt");
+    }
 
     @Test
     void loadMap_LoadDummyMap_NoExceptionsThrown() {
@@ -12,7 +22,6 @@ public class MapLoaderTest {
 
     @Test
     void loadMap_LoadDummyMap_EveryCellHasProperTile() {
-        GameMap gameMap = MapLoader.loadMap("/testmap.txt");
         Cell empty = gameMap.getCell(0, 0);
         Cell floor = gameMap.getCell(3, 1);
         Cell wall = gameMap.getCell(1, 1);
@@ -29,4 +38,37 @@ public class MapLoaderTest {
         assertEquals(CellType.HEARTONFLOOR, heartOnFloor.getType());
     }
 
+    @Test
+    void loadMap_LoadDummyMap_ItemsAreOnRightPlaces() {
+        Cell floorEmpty = gameMap.getCell(3, 1);
+        Cell key = gameMap.getCell(2, 3);
+        Cell sword = gameMap.getCell(3, 3);
+        assertNull(floorEmpty.getItem());
+        assertNotNull(key.getItem());
+        assertNotNull(sword);
+    }
+
+    @Test
+    void loadMap_LoadDummyMap_EnemiesAreOnSpot() {
+        Cell floorEmpty = gameMap.getCell(3, 1);
+        Cell skeleton = gameMap.getCell(2, 2);
+        Cell wizard = gameMap.getCell(3, 2);
+        Cell golem = gameMap.getCell(4, 4);
+        assertNull(floorEmpty.getActor());
+        assertNotNull(skeleton.getActor());
+        assertNotNull(wizard.getActor());
+        assertNotNull(golem.getActor());
+    }
+
+    @Test
+    void loadMap_LoadDummyMap_PlayerIsOnSpot() {
+        Cell playerCell = gameMap.getCell(2, 1);
+        assertNotNull(playerCell.getActor());
+        assertNotNull(gameMap.getPlayer());
+    }
+
+    @Test
+    void loadMap_LoadInvalidMap_RunTimeExceptionThrown() {
+        assertThrows(RuntimeException.class, () -> MapLoader.loadMap("/invalidtestmap.txt"));
+    }
 }
