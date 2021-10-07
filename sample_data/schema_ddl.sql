79ae395c5fea0399d49ba0927bc1ddaf29dba8d9
Balint Kozari <mrkoba@gmail.com>
2021-10-07 12:37:19 +0200

    fix: extend sql file #2

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -1,10 +1,18 @@
+DROP TABLE IF EXISTS public.cell;
+DROP TABLE IF EXISTS public.map;
+DROP TABLE IF EXISTS public.enemy;
+DROP TABLE IF EXISTS public.player_item;
+DROP TABLE IF EXISTS public.item;
+DROP TABLE IF EXISTS public.player;
 DROP TABLE IF EXISTS public.game_state;
+
+
+
 CREATE TABLE public.game_state (
     id integer UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     saved_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-DROP TABLE IF EXISTS public.map;
 CREATE TABLE public.map (
     id integer UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     width integer NOT NULL,
@@ -12,7 +20,6 @@ CREATE TABLE public.map (
     state_id integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public.cell;
 CREATE TABLE public.cell(
     id integer UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     type varchar NOT NULL,
@@ -23,20 +30,17 @@ CREATE TABLE public.cell(
     item_id integer
 );
 
-DROP TABLE IF EXISTS public.enemy;
 CREATE TABLE public.enemy(
     id integer UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     hp integer NOT NULL,
     type varchar NOT NULL
 );
 
-DROP TABLE IF EXISTS public.item;
 CREATE TABLE public.item(
     id integer UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar NOT NULL
 );
 
-DROP TABLE IF EXISTS public.player;
 CREATE TABLE public.player (
     id integer UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name text NOT NULL,
@@ -46,7 +50,6 @@ CREATE TABLE public.player (
     state_id integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public.player_item;
 CREATE TABLE public.player_item (
     id integer UNIQUE PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     player_id integer NOT NULL,
@@ -67,3 +70,7 @@ ALTER TABLE ONLY public.player
 ALTER TABLE ONLY public.player_item
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id),
     ADD CONSTRAINT fk_item_id FOREIGN KEY (item_id) REFERENCES public.item(id);
+
+INSERT INTO item (name)
+VALUES ('sword'),
+       ('key');
