38f0f5f2c498e05703e025ee01f8dfccf90f58b3
Balint Kozari <mrkoba@gmail.com>
2021-10-07 20:18:33 +0200

    fix: add missing arguments

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDao.java
@@ -1,12 +1,14 @@
 package com.codecool.dungeoncrawl.dao;
 
+import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.model.GameState;
+import com.codecool.dungeoncrawl.model.PlayerModel;
 
 import java.util.List;
 
 public interface GameStateDao {
     void add(GameState state);
     void update(GameState state);
-    GameState get(int id);
+    GameState get(int id, PlayerModel player);
     List<GameState> getAll();
 }
