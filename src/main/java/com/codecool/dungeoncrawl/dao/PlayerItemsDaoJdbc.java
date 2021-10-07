cdd65c6bd8598de8e7ef8fb3642edb5c52273546
szirakibalint <blntsziraki@gmail.com>
2021-10-07 13:53:42 +0200

    feat: get items for player with PlayerItemsDaoJdbc #1

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerItemsDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerItemsDaoJdbc.java
@@ -1,9 +1,16 @@
 package com.codecool.dungeoncrawl.dao;
 
 import com.codecool.dungeoncrawl.logic.items.Item;
+import com.codecool.dungeoncrawl.logic.items.Key;
+import com.codecool.dungeoncrawl.logic.items.Sword;
 import com.codecool.dungeoncrawl.logic.util.Inventory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerItemsDaoJdbc implements PlayerItemsDao {
@@ -24,6 +31,28 @@ public class PlayerItemsDaoJdbc implements PlayerItemsDao {
 
     @Override
     public List<Item> get(int player_id) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = """
+                    SELECT item.id, item.name
+                    FROM item
+                    LEFT JOIN player_item pi ON item.id = pi.item_id
+                    LEFT JOIN player p ON p.id = pi.player_id
+                    WHERE player_id = ?
+                    """;
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, player_id);
+            ResultSet resultSet = conn.createStatement().executeQuery(sql);
+            List<Item> items = new ArrayList<>();
+            while (resultSet.next()) {
+                String itemName = resultSet.getString(2);
+                switch (itemName) {
+                    case "sword" -> items.add(new Sword());
+                    default -> items.add(new Key());
+                }
+            }
+            return items;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
